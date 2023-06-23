946cc15c012a54a204e1a127f8b429e6a75b1783
Choishroy <101175325+Choishroy@users.noreply.github.com>
2023-06-23 14:41:39 +0900

    final test

--- Exercise-7-16.js
+++ Exercise-7-16.js
@@ -1,17 +1,38 @@
-var Rectangle = class {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-    }
-    getArea() {
-      return this.width * this.height;
-    }
-  };
-  var Square = class extends Rectangle {
-    constructor(width) {
-      super(width, width);
-    }
-    getArea() {
-      console.log('size is :', super.getArea());
-    }
-  };
+fs = require('fs');
+console.log('Start');
+process.nextTick(() => {
+    console.log(`First next-tick callback executed`);
+});
+Promise.resolve('resolved').then((result) => {
+    console.log(`First promise was ${result}`);
+});
+fs.readFile('test.txt', (error) => {
+    setTimeout(() => {
+        console.log('Timeout callback executed');
+  }, 0);
+    setImmediate(() => {
+        console.log('Immediate callback executed');
+  });
+  if (!error) {
+    console.log('File read');
+  }
+  Promise.resolve('resolved').then((result) => {
+    console.log(`Second promise was ${result}`);
+  });
+  process.nextTick(() => {
+    console.log(`Second next-tick callback executed`);
+  });
+  })
+  Promise.resolve('resolved').then((result) => {
+    console.log(`Third promise was ${result}`);
+  });
+  process.nextTick(() => {
+    console.log(`Third next-tick callback executed`);
+  });
+  console.log('End');
+  process.on('exit', (exitCode) => {
+    console.log(`Process exited with code ${exitCode}`);
+});
+
+
+        
